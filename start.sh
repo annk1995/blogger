56d7ae6a988c6446b9c1cd104981c833faa33642
annk1995 <kabuikariuki052@gmail.com>
2022-05-18 22:53:36 +0300

    slm

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-    export EMAIL_USER='kimborne7@gmail.com'
+    export EMAIL_USER='kabuikariuki052@gmail.com'
     export EMAIL_PASS='borauhai@com'
     export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
     export SECRET_KEY='f9bf78b9a18ce6d46a0cd2b0b86df9da'
